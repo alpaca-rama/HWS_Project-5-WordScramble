b5fb4a6e7dcf49b49d7b0919f9b12143d3620397
Luca <luca@weve-got-it.com>
2022-08-13 14:47:35 +0200

    Initial Commit

---
+++ Project-5-WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-5-WordScramble
+//
+//  Created by Luca Capriati on 2022/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
