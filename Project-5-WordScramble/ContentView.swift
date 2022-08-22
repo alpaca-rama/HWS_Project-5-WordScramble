ced82565c5e7f6ce37418e059394de202df071dc
Luca <luca@weve-got-it.com>
2022-08-22 15:40:51 +0200

    Revised: Going over all the projects to fix everything and re do challenges.

--- Project-5-WordScramble/ContentView.swift
+++ Project-5-WordScramble/ContentView.swift
@@ -4,6 +4,11 @@
 //
 //  Created by Luca Capriati on 2022/08/13.
 //
+// Challanges:
+// Project 5 - Challange 1: Disallow answers that are shorter than three letters or are just our start word.
+// Project 5 - Challange 2: Add a toolbar button that calls startGame(), so users can restart with a new word whenever they want to.
+// Project 5 - Challange 3: Put a text view somewhere so you can track and show the player’s score for a given root word.
+//  How you calculate score is down to you, but something involving number of words and their letter count would be reasonable.
 
 import SwiftUI
 
@@ -13,14 +18,10 @@ struct ContentView: View {
     @State private var newWord = ""
     @State private var errorTitle = ""
     @State private var errorMessage = ""
-    @State private var isErrorShowing = false
-    // Challange 3
+    @State private var showingError = false
+    // Project 5 - Challange 1
     @State private var score = 0
-    // Challange 3 - My Addition
-    @State private var scoreAddedAmount = 0
-    
-    private let minWordSize = 3
-    
+
     var body: some View {
         NavigationView {
             List {
@@ -28,116 +29,103 @@ struct ContentView: View {
                     TextField("Enter your word", text: $newWord)
                         .autocapitalization(.none)
                 }
-                
+
                 Section {
                     ForEach(usedWords, id: \.self) { word in
                         HStack {
                             Image(systemName: "\(word.count).circle")
-//                                .foregroundColor(wordScoreColor == "blue" ? .red : .black)
-                                .foregroundColor(word.count == 3 ? .red : word.count == 4 ? .red : word.count == 5 ? .blue : word.count == 6 ? .blue : word.count == 7 ? .green : word.count == 8 ? .indigo : .black)
+                                // my own extra
+                                .foregroundColor(word.count == 3 ? .red : word.count == 4 ? .orange : word.count == 5 ? .yellow : word.count == 6 ? .green : word.count == 7 ? .blue : .indigo)
                             Text(word)
                         }
                     }
                 }
-                
-                Section() {
-                    HStack {
-                        Text("Score: \(score)")
-                            .font(.title)
-                        // My own added code
-                        Text("(+\(scoreAddedAmount))")
-                            .foregroundColor(scoreAddedAmount == 1 ? .red : scoreAddedAmount == 2 ? .blue : scoreAddedAmount == 3 ? .green : scoreAddedAmount == 10 ? .indigo : .white)
-                            
-                    }
-                    
-                }
-            }
-            // Challange 2
-            .toolbar {
-                Button("Restart", action: startGame)
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
-            .alert(errorTitle, isPresented: $isErrorShowing) {
+            .alert(errorTitle, isPresented: $showingError) {
                 Button("OK", role: .cancel) { }
             } message: {
                 Text(errorMessage)
             }
+            // Project 5 - Challange 2
+            .toolbar {
+                Button("New Game", action: startGame)
+            }
+            // Project 5 - Extra
+            .safeAreaInset(edge: .bottom) {
+                Text("Score: \(score)")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .font(.title)
+            }
         }
     }
-    
+
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        guard answer.count > 0 else {
-            wordError(title: "No word entered", message: "There must be characters entered (Minimum of 3 charaters)")
+        // Project 5 - Challange 1
+        guard answer.count >= 3 else {
+            wordError(title: "Word too short", message: "Words must be at least three letters long.")
             return
         }
-        
-        // Challange 1
-        guard isLongEnough(word: answer) else {
-            wordError(title: "Word is too short", message: "The word must be 3 or more characters.")
+        // Project 5 - Challange 1
+        guard answer != rootWord else {
+            wordError(title: "Nice try...", message: "You can't user your starting word!")
             return
         }
-        
-        // Challange 1
-        guard isNotStartingWord(word: answer) else {
-            wordError(title: "Same as starting word", message: "The word cannot be the starting word, '\(rootWord)'")
-            return
-        }
-        
+
         guard isOriginal(word: answer) else {
             wordError(title: "Word used already", message: "Be more original!")
             return
         }
-        
+
         guard isPossible(word: answer) else {
             wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
             return
         }
-        
+
         guard isReal(word: answer) else {
-            wordError(title: "Word not recognised", message: "You can't just make them up, you know!")
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
             return
         }
-        
+
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
-        
-        updateScore(word: answer)
-        
+
         newWord = ""
+        // Project 5 - Challange 3
+        score += answer.count
     }
-    
+
     func startGame() {
-        // Challange 2
-        withAnimation {
-            usedWords.removeAll()
-        }
-        // My added code
-        scoreAddedAmount = 0
-        
+        // Project 5 - Challange 2
+        newWord = ""
+        usedWords.removeAll()
+        // Project 5 - Challange 3
+        score = 0
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "silkworm"
-                
                 return
             }
         }
-        
-        fatalError("Could not load start.txt from bundle")
+
+        fatalError("Could not load start.txt from bundle.")
     }
-    
+
     func isOriginal(word: String) -> Bool {
         !usedWords.contains(word)
     }
-    
+
     func isPossible(word: String) -> Bool {
         var tempWord = rootWord
-        
+
         for letter in word {
             if let pos = tempWord.firstIndex(of: letter) {
                 tempWord.remove(at: pos)
@@ -145,53 +133,22 @@ struct ContentView: View {
                 return false
             }
         }
-        
+
         return true
     }
-    
+
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        
+
         return misspelledRange.location == NSNotFound
     }
-    
-    // Challange 1
-    func isLongEnough(word: String) -> Bool {
-        return word.count >= minWordSize
-    }
-    
-    // Challange 1
-    func isNotStartingWord(word: String) -> Bool {
-        return word != rootWord
-    }
-    
-    
+
     func wordError(title: String, message: String) {
         errorTitle = title
         errorMessage = message
-        isErrorShowing = true
-    }
-    
-    func updateScore(word: String) {
-        switch word.count {
-        case 3...4:
-            scoreAddedAmount = 1
-            score += scoreAddedAmount
-        case 5:
-            scoreAddedAmount = 2
-            score += scoreAddedAmount
-        case 6...7:
-            scoreAddedAmount = 3
-            score += scoreAddedAmount
-        case 8:
-            scoreAddedAmount = 10
-            score += scoreAddedAmount
-        default:
-            scoreAddedAmount = 0
-            score += scoreAddedAmount
-        }
+        showingError = true
     }
 }
 
